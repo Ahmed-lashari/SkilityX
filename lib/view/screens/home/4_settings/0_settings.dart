e18cfc1d124b818893dd7915e0d152c8488f9ba0
Ahmed-lashari <ahmedlashari.official@gmail.com>
2025-05-15 22:23:03 +0500

    settings screen created + readme file updated

--- lib/view/screens/home/4_settings/0_settings.dart
+++ lib/view/screens/home/4_settings/0_settings.dart
@@ -1,11 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:skility_x/constants/app-icons.dart';
+import 'package:skility_x/constants/app_colors.dart';
+import 'package:skility_x/core/config/route_config.dart';
+import 'package:skility_x/view/screens/home/4_settings/cover_photo_setting.dart';
+import 'package:skility_x/view/themes/theme_conts/typography.dart';
 import 'package:skility_x/view/widgets/custom_scaffold.dart';
+import 'package:skility_x/view/widgets/custom_widgets.dart';
+import 'package:skility_x/view/widgets/image_ui.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold();
+    return CustomScaffold(
+      appBar: AppBar(
+        title: Text(
+          'Settings',
+          style: TextStyle(fontFamily: AppTypography.scotishMedium),
+        ),
+        leading: CustomWidgets.backNavigationIconButt(context: context),
+      ),
+      body: Column(
+        spacing: 10,
+        children: [
+          SizedBox.shrink(),
+          _SettingsTileCard(
+              onTap: () =>
+                  AppNavigator.navigateTo(context, wRoute: CoverPhotoSetting()),
+              leadingIcon: AppImageIcons.camera,
+              title: "Cover Photos"),
+          _SettingsTileCard(
+              onTap: () {},
+              leadingIcon: AppImageIcons.camera,
+              title: "Log Out"),
+        ],
+      ),
+    );
+  }
+}
+
+class _SettingsTileCard extends StatelessWidget {
+  final String leadingIcon;
+  final String title;
+  final VoidCallback? onTap;
+
+  const _SettingsTileCard({
+    required this.leadingIcon,
+    required this.title,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      type: MaterialType.transparency,
+      child: InkWell(
+        onTap: onTap,
+        splashColor: AppColors.onBackground,
+        borderRadius: BorderRadius.circular(12),
+        child: Ink(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: AppColors.unselectedItemIcon),
+          child: ListTile(
+            // contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            leading: CustomIcon(
+              icon: leadingIcon,
+              color: AppColors.secondary,
+              iconSize: 30,
+            ),
+            title: Text(
+              title,
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontFamily: AppTypography.scotishMedium),
+            ),
+            trailing: CustomIcon(
+              icon: AppImageIcons.rightarrow,
+              color: AppColors.secondary,
+              iconSize: 20,
+            ),
+            onTap: null,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/view/screens/splash/splash.dart
+++ lib/view/screens/splash/splash.dart
@@ -1,8 +1,7 @@
-// import 'dart:async';
-
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:skility_x/constants/app_colors.dart';
-// import 'package:skility_x/core/config/route_config.dart';
+import 'package:skility_x/constants/app-icons.dart';
+import 'package:skility_x/core/config/route_config.dart';
 import 'package:skility_x/view/widgets/custom_scaffold.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -16,25 +15,18 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-              spacing: 30,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/skility_x_icon/skility_x.png', scale: 13),
-                Text(
-                  "SkilityX",
-                  style: TextStyle(
-                      fontSize: 40,
-                      color: AppColors.secondary,
-                      fontWeight: FontWeight.bold),
-                )
-              ])
-        ],
-      ),
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox.shrink(),
+            Image.asset(AppImageIcons.appLogo, scale: 13),
+            Text("by: Muhammad Ahmed Lashari", style: TextStyle(fontSize: 20))
+          ],
+        ),
+      )),
     );
   }
 
@@ -42,9 +34,9 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
 
-    // Timer(
-    //     Duration(seconds: 1),
-    //     () => AppNavigator.navigateTo(context,
-    //         sRoute: RouteEnum.authScreen.path));
+    Timer(
+        Duration(seconds: 1),
+        () => AppNavigator.navigateTo(context,
+            sRoute: RouteEnum.authScreen.path));
   }
 }
